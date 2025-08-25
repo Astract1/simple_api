11d9b726dc59fc607277144b6f655d7634e0ac75
Astract <diegojimenez2432@gmail.com>
2025-08-24 19:07:35 -0500

    Actualiza la dirección IP del host en la configuración de uvicorn a 127.0.0.1.

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ def eliminar_libro(libro_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
